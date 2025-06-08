793d413f286ee3d14f3c99748408cdf34c384db7
Askingbl <cofandureno10@gmail.com>
2025-06-08 13:27:38 -0500

    nuevo

--- obj/Debug/net9.0/prueba1.AssemblyInfo.cs
+++ obj/Debug/net9.0/prueba1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prueba1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51781492784216c5fb3f0526d44de7b6dd185a51")]
 [assembly: System.Reflection.AssemblyProductAttribute("prueba1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prueba1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
